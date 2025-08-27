463a747fd37d8613bb737f39c02ba05839879920
Your Name <your-email@example.com>
2025-08-27 16:56:40 -0400

    completed switch challenge

--- src/SwitchTest.java
+++ src/SwitchTest.java
@@ -1,2 +1,24 @@
 public class SwitchTest {
+    public static void main(String[] args) {
+
+        printNumberInWord(4);
+    }
+        public static void printNumberInWord(int number){
+        String numberInWords = switch (number){
+            case 0 -> {
+                yield
+             "ZERO";}
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+            System.out.println(numberInWords);
+    }
 }
